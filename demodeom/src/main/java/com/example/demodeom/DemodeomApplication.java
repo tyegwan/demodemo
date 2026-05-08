80edf9828772c8c68e536326e08337bed79d3158
tyegwan <tye4703@naver.com>
2026-05-08 11:08:33 +0900

    first commit

---
+++ demodeom/src/main/java/com/example/demodeom/DemodeomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodeomApplication.class, args);
+	}
+
+}
